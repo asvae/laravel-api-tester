523938401e53b74d58627b051ec5905711ffc3e9
Asvae <ontrew@gmail.com>
2016-07-28 01:43:50 +0300

    Detach request field.

---
+++ resources/assets/js/request.vue
@@ -0,0 +1,60 @@
+<template>
+    <li transition="fade-in"
+        class="request"
+        :class="{selected: currentRequest === request}"
+    >
+        <a>
+            <div class="columns">
+                <div class="column is-narrow">
+                    <button class="button is-small is-active"
+                            @click="setCurrentRequest(request), scheduleRequest(true)"
+                            v-text="request.method"
+                    ></button>
+                </div>
+                <a @click="setCurrentRequest(request)"
+                   class="column is-bold"
+                   v-text="displayedName"
+                   style="white-space: nowrap"
+                ></a>
+                <a class="column is-narrow"
+                   v-text="'X'"
+                   @click="deleteRequest(request)"
+                ></a>
+            </div>
+        </a>
+    </li>
+</template>
+
+<script>
+    import * as actions from './vuex/actions.js'
+
+    export default {
+        data (){
+            return {}
+        },
+        vuex: {
+            actions,
+        },
+        props: {
+            request: {
+                type: Object,
+            }
+        },
+        computed: {
+            displayedName (){
+                return this.request.name ? this.request.name : this.request.path
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .request.selected {
+        border-left: 2px solid rgb(255, 82, 82);
+        background-color: #eef9f2;
+    }
+
+    .request {
+        border-left: 2px solid transparent;
+    }
+</style>
