283b96d5cc81ef7052ce1e8736448d1bb40ba183
Asvae <ontrew@gmail.com>
2016-07-28 01:41:59 +0300

    Detach json editor and make json viewer.

---
+++ resources/assets/js/json-viewer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    import $ from 'jquery'
+    import jsonEditor from './jsonEditorMixin'
+
+    export default {
+        extends: jsonEditor,
+
+        watch: {
+            json (json){
+                this.$options.editor.set(json)
+            }
+        },
+        ready() {
+            let options = {
+                mode: 'view'
+            }
+
+            this.$options.editor = new JSONEditor(this.$el, options, this.json)
+        },
+    }
+</script>
+
+<style>
+
+</style>
