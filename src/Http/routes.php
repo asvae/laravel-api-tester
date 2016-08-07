0fe299dbc1fbdc2f155d3745a92491f2519050dd
Greabock <greabock@gmail.com>
2016-08-07 22:12:50 +0700

    Leadinf underscore for no-collision with user's app

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -13,29 +13,30 @@ Route::resource('routes', 'RouteController', [
 ]);
 
 // There we can get list of requests, store new request, update existing or destroy it.
-// Route parameter renamed with leading underscores for no-collision with user's app.
+// Route parameter renamed with leading underscore for no-collision with user's app.
 Route::resource('requests', 'RequestController', [
     'only'       => ['index', 'store', 'update', 'destroy'],
-    'parameters' => ['requests' => 'request']
+    'parameters' => ['requests' => '_request']
 ]);
 
 // Assets resources have no publish because this package just a service for developers.
 // So, we can to route requests for files trough AssetsController.
+// Route parameter named with leading underscore for no-collision with user's app.
 Route::group(['prefix' => 'assets'], function () {
 
     // Define a file pattern
     $filePattern = '^([a-z_\-\.]+)$';
 
-    Route::get('img/{file}', ['as' => 'image', 'uses' => 'AssetsController@image'])
-         ->where('file', $filePattern);
+    Route::get('img/{_file}', ['as' => 'image', 'uses' => 'AssetsController@image'])
+         ->where('_file', $filePattern);
 
-    Route::get('{file}', ['as' => 'file', 'uses' => 'AssetsController@index'])
-         ->where('file', $filePattern);
+    Route::get('{_file}', ['as' => 'file', 'uses' => 'AssetsController@index'])
+         ->where('_file', $filePattern);
 });
 
 
 // I actually dunno what this route doing here... it isn't my.
-Route::get('test-routes/{type}', 'HomeController@testRoutes');
+Route::get('test-routes/{_type}', 'HomeController@testRoutes');
 
 
 
