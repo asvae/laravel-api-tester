fb8a098ffa95500e5b948a8e87039eb020790750
Greabock <greabock@gmail.com>
2016-08-14 21:16:31 +0700

    Headers for dingo-route

--- src/Repositories/RouteDingoRepository.php
+++ src/Repositories/RouteDingoRepository.php
@@ -6,6 +6,8 @@ use Asvae\ApiTester\Collections\RouteCollection;
 use Asvae\ApiTester\Contracts\RouteRepositoryInterface;
 use Asvae\ApiTester\Entities\RouteInfo;
 use Dingo\Api\Routing\Router;
+use Illuminate\Contracts\Config\Repository as Config;
+use Illuminate\Contracts\Foundation\Application;
 
 /**
  * Class RouteDingoRepository
@@ -19,13 +21,25 @@ class RouteDingoRepository implements RouteRepositoryInterface
      */
     protected $routes;
 
-    public function __construct(RouteCollection $collection, Router $router)
+    public function __construct(RouteCollection $collection, Router $router, Config $config)
     {
         $this->routes = $collection;
+        $standardsTree = $config['api.standardsTree'];
+        $subtype = $config['api.subtype'];
+        $defaultFormat = $config['api.defaultFormat'];
 
         foreach ($router->getAdapterRoutes() as $versionName => $versionGroup) {
             foreach ($versionGroup as $route) {
-                $routeInfo = (new RouteInfo($route, ['router' => 'Dingo', 'version' => $versionName]))->toArray();
+                $routeInfo = (new RouteInfo($route, [
+                    'router' => 'Dingo',
+                    'version' => $versionName,
+                    'headers' => [
+                        [
+                            'key' => 'Accept',
+                            'value' => "application/{$standardsTree}.{$subtype}.{$versionName}+{$defaultFormat}"
+                        ]
+                    ]
+                ]))->toArray();
                 $this->routes->push($routeInfo);
             }
         }
