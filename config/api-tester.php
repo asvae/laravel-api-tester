9216bc06d0bf82d7a4476570bbee90fe2664ea67
Greabock <greabock@gmail.com>
2016-08-16 23:21:01 +0700

    Base Middlewares

--- config/api-tester.php
+++ config/api-tester.php
@@ -39,11 +39,11 @@ return [
     */
 
     'middleware' => [
-        App\Http\Middleware\EncryptCookies::class,
+        Illuminate\Cookie\Middleware\EncryptCookies::class,
         Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         Illuminate\Session\Middleware\StartSession::class,
         Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        App\Http\Middleware\VerifyCsrfToken::class,
+        Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
     ],
 
 
