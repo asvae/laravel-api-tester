c98183f0304f6db36cb37a624576984635431ca9
Greabock <greabock@gmail.com>
2016-08-16 21:10:47 +0700

    Gorup

--- config/api-tester.php
+++ config/api-tester.php
@@ -38,7 +38,13 @@ return [
     |
     */
 
-    'middleware' => [],
+    'middleware' => [
+        App\Http\Middleware\EncryptCookies::class,
+        Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        Illuminate\Session\Middleware\StartSession::class,
+        Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        App\Http\Middleware\VerifyCsrfToken::class,
+    ],
 
 
     /*
