0132f11e0b09afe8c41b21e75565727762600dad
Asvae <ontrew@gmail.com>
2016-09-08 15:05:46 +0300

    Use container instead of facade alias.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,35 +1,37 @@
 <?php
 
+$router = app('router');
+
 // Api-tester base route. This is entry point for frontend-SPA.
-Route::get('/', [
+$router->get('/', [
     'as'   => 'home',
     'uses' => 'HomeController@index',
 ]);
 
 // Fetch all Laravel routes.
-Route::post('routes/index', 'RouteController@index');
+$router->post('routes/index', 'RouteController@index');
 
-Route::post('requests/index', 'RequestController@index');
-Route::post('requests/store', 'RequestController@store');
-Route::post('requests/update', 'RequestController@update');
-Route::post('requests/destroy', 'RequestController@destroy');
+$router->post('requests/index', 'RequestController@index');
+$router->post('requests/store', 'RequestController@store');
+$router->post('requests/update', 'RequestController@update');
+$router->post('requests/destroy', 'RequestController@destroy');
 
 // We won't publish library's assets.
 // Instead we'll pass them via app which is slower but fine for development.
-Route::group(['prefix' => 'assets'], function () {
+$router->group(['prefix' => 'assets'], function ($router) {
 
     $filePattern = '^([a-z0-9_\-\.]+)$';
 
-    Route::get('fonts/{_file}', [
+    $router->get('fonts/{_file}', [
         'as' => 'image',
         'uses' => 'AssetsController@font'
     ])->where('_file', $filePattern);
 
-    Route::get('img/{_file}', [
+    $router->get('img/{_file}', [
         'as' => 'image', 'uses' => 'AssetsController@image'
     ])->where('_file', $filePattern);
 
-    Route::get('{_file}', [
+    $router->get('{_file}', [
         'as' => 'file',
         'uses' => 'AssetsController@index'
     ])->where('_file', $filePattern);
@@ -44,7 +46,7 @@ Route::group(['prefix' => 'assets'], function () {
  * This route is debug only, hence in production
  * it isn't registered and route cache is allowed.
  */
-Route::any('* routes should not be cached',[
+$router->any('* routes should not be cached',[
     'as' => 'routes-should not be cached',
     'uses' =>  function () { return 'Api-tester routes-should not be cached';},
 ]);
